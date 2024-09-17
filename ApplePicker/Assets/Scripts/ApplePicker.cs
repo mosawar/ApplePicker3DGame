d1376e79b2916e2fa6b3b3179e5d5ce298723cf5
mosawar <94396496+mosawar@users.noreply.github.com>
2024-09-17 16:09:34 -0700

    basket destroy added

--- ApplePicker/Assets/Scripts/ApplePicker.cs
+++ ApplePicker/Assets/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,16 +10,20 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketbSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
+
         for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketbSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
 
@@ -32,15 +37,24 @@ public class ApplePicker : MonoBehaviour
         {
             Destroy(tempGO);
         }
-    }
 
+        //destroy one of the baskets
 
+        // get the index of the last basket in basketlist
+        int basketIndex = basketList.Count - 1;
 
+        //get a reference to that basket gameobject
+        GameObject basketGo = basketList[basketIndex];
 
+        //remove basket from the list and destory the gameobkject
+        basketList.RemoveAt(basketIndex);
+        Destroy(basketGo);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // if there are no baskets left, restart the game
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("_Scene_0");
+        }
+
     }
 }
